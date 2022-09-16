98e54555a35c648ba18a3e10dd3cea9a7901b7a2
Sumi Seo <seo.sumi@favoritemedium.com>
2022-09-16 14:24:17 +0900

    feat(detail): create detail pages

--- pages/ninjas/[id].jsx
+++ pages/ninjas/[id].jsx
@@ -1,5 +1,5 @@
 export const getStaticPaths = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/");
   const data = await res.json();
   const paths = data.map((ninja) => {
     return {
@@ -14,10 +14,21 @@ export const getStaticPaths = async () => {
   };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+  return {
+    props: { ninja: data },
+  };
+};
+
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h1>Details page</h1>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
     </div>
   );
 };
